1380f181be64fff249ae304a17227758390e118f
Devon Govett <devongovett@gmail.com>
2021-11-09 10:22:51 -0500

    Add playground

---
+++ playground/playground.js
@@ -0,0 +1,30 @@
+import init, {transform} from '../pkg';
+
+let enc = new TextEncoder();
+let inputs = document.querySelectorAll('input[type=number]');
+
+async function update() {
+  await init();
+
+  let targets = {};
+  for (let input of inputs) {
+    if (input.value !== '') {
+      targets[input.id] = input.valueAsNumber << 16;
+    }
+  }
+
+  let res = transform({
+    filename: 'test.css',
+    code: enc.encode(source.value),
+    minify: minify.checked,
+    targets
+  });
+
+  compiled.value = res;
+}
+
+update();
+source.oninput = update;
+for (let input of document.querySelectorAll('input')) {
+  input.oninput = update;
+}
