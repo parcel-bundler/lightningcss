cc17ad51910c3a55788e14e9c2e7973a096a7095
Jan Schulte <janschulte@fastmail.com>
2022-01-14 20:33:29 -0500

    reorganize main function
    Removing unused imports and functions.
    Also moves minify handler into separate function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::{self, File}, io::{Read, Write}, path::Path};
+use std::{fs::{self, File}, io::{Read, Write}};
 
 use clap::{Parser, Args};
 use parcel_css::stylesheet::{StyleSheet, MinifyOptions, PrinterOptions, ParserOptions};
@@ -22,36 +22,30 @@ struct MinifyArgs {
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     match Cli::parse() {
         Cli::Minify(cli_args) => {
-            let source = read_file(&cli_args.input_file)?;
-            let mut stylesheet = StyleSheet::parse(cli_args.input_file.to_string(), &source, ParserOptions::default()).unwrap();
-            stylesheet.minify(MinifyOptions::default());
-            let res = stylesheet.to_css(PrinterOptions { minify: true, ..PrinterOptions::default() }).unwrap();
-
-            if let Some(output_file) = cli_args.output_file {
-                write_file(&output_file, &res.code)?;
-            }
-            else {
-                println!("{}", res.code);
-            }
+            minify_css(cli_args)?;
         }
     }
 
     Ok(())
 }
 
+fn minify_css(cli_args: MinifyArgs) -> Result<(), Box<dyn std::error::Error>> {
+    let source = read_file(&cli_args.input_file)?;
+    let mut stylesheet = StyleSheet::parse(cli_args.input_file.to_string(), &source, ParserOptions::default()).unwrap();
+    stylesheet.minify(MinifyOptions::default());
+    let res = stylesheet.to_css(PrinterOptions { minify: true, ..PrinterOptions::default() }).unwrap();
+    Ok(if let Some(output_file) = cli_args.output_file {
+        write_file(&output_file, &res.code)?;
+    }
+    else {
+        println!("{}", res.code);
+    })
+}
+
 fn write_file(output_filename: &str, file_content: &str) -> Result<(), std::io::Error> {
     let mut file = File::create(output_filename)?;
     file.write_all(file_content.as_bytes())?;
     Ok(())
-}
-
-fn generate_output_filename(input_file: &str) ->String {
-    let path = Path::new(input_file);
-
-    let extension = path.extension().unwrap();
-    let base_name = path.file_stem().unwrap();
-
-    format!("{}.minified.{}", base_name.to_str().unwrap(), extension.to_str().unwrap())
 }
 
 fn read_file(filename: &str) -> Result<String, std::io::Error> {
