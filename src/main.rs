54bec9caf281703a2548660aede4ed55fa36bf34
rob cobb <rwcobbjr@gmail.com>
2022-01-17 18:12:38 -0500

    refactor minify to a bool option, input file as required positional arg

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,33 @@
 use std::fs;
-use clap::{Args, Parser};
+use clap::Parser;
 use parcel_css::stylesheet::{MinifyOptions, ParserOptions, PrinterOptions, StyleSheet};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
-enum Cli {
-  /// Minify a given CSS file
-  Minify(MinifyArgs),
-}
-
-#[derive(Args, Debug)]
-struct MinifyArgs {
+struct CliArgs {
   /// The CSS file to minify
   input_file: String,
-  /// The name of the output file
+  /// Minify the output 
+  #[clap(short, long)]
+  minify: bool,
+    /// The destination file for the output
   #[clap(short, long)]
   output_file: Option<String>,
 }
 
 pub fn main() -> Result<(), std::io::Error> {
-  match Cli::parse() {
-    Cli::Minify(cli_args) => {
-      minify_css(cli_args)
-    }
-  }
-}
-
-fn minify_css(cli_args: MinifyArgs) -> Result<(), std::io::Error> {
+  let cli_args = CliArgs::parse();
   let source = fs::read_to_string(&cli_args.input_file)?;
   let mut stylesheet = StyleSheet::parse(cli_args.input_file.to_string(), &source, ParserOptions::default()).unwrap();
-  stylesheet.minify(MinifyOptions::default()).unwrap();
-  let res = stylesheet.to_css(PrinterOptions { minify: true, ..PrinterOptions::default()}).unwrap();
+
+  if cli_args.minify {
+    stylesheet.minify(MinifyOptions::default()).unwrap();
+  }
+
+  let res = stylesheet.to_css(PrinterOptions { 
+    minify: cli_args.minify, 
+    ..PrinterOptions::default()
+  }).unwrap();
 
   if let Some(output_file) = cli_args.output_file {
     fs::write(output_file, res.code.as_bytes())?;
