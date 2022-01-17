d64d05db0adfc0430c3ca4aa81f530746cc6de21
rob cobb <rwcobbjr@gmail.com>
2022-01-17 18:21:59 -0500

    add option to enable css nesting

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,29 @@ use clap::Parser;
 use parcel_css::stylesheet::{MinifyOptions, ParserOptions, PrinterOptions, StyleSheet};
 
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[clap(author, about, long_about = None)]
 struct CliArgs {
   /// The CSS file to minify
   input_file: String,
   /// Minify the output 
   #[clap(short, long)]
   minify: bool,
-    /// The destination file for the output
+  /// The destination file for the output
   #[clap(short, long)]
   output_file: Option<String>,
+  /// Enable CSS nesting in output
+  #[clap(short, long)]
+  nesting: bool,
 }
 
 pub fn main() -> Result<(), std::io::Error> {
   let cli_args = CliArgs::parse();
   let source = fs::read_to_string(&cli_args.input_file)?;
-  let mut stylesheet = StyleSheet::parse(cli_args.input_file.to_string(), &source, ParserOptions::default()).unwrap();
+
+  let mut stylesheet = StyleSheet::parse(cli_args.input_file.to_string(), &source, ParserOptions {
+    nesting: cli_args.nesting,
+    ..ParserOptions::default()
+  }).unwrap();
 
   if cli_args.minify {
     stylesheet.minify(MinifyOptions::default()).unwrap();
