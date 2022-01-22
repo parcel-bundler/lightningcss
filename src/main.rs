1c941608b61c3c52e6f921330ec54f73e0cc9021
Devon Govett <devongovett@gmail.com>
2022-01-22 12:10:07 -0500

    Run rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,31 @@
 use clap::Parser;
 use parcel_css::stylesheet::{MinifyOptions, ParserOptions, PrinterOptions, StyleSheet};
-use std::{ffi, fs, io, path};
 use serde::Serialize;
+use std::{ffi, fs, io, path};
 
 #[derive(Parser, Debug)]
 #[clap(author, about, long_about = None)]
 struct CliArgs {
-    /// Target CSS file
-    input_file: String,
-    /// Destination file for the output
-    #[clap(short, long, group = "output_file")]
-    output_file: Option<String>,
-    /// Minify the output
-    #[clap(short, long)]
-    minify: bool,
-    /// Enable parsing CSS nesting
-    #[clap(short, long)]
-    nesting: bool,
-    /// Enable CSS modules in output
-    #[clap(short, long, group = "css_modules")]
-    css_modules: bool,
-    /// Default: <output_file>.json
-    #[clap(long, requires = "css_modules")]
-    css_modules_output_file: Option<String>,
-    /// Enable sourcemap, at <output_file>.map
-    #[clap(short, long, requires = "output_file")]
-    sourcemap: bool,
+  /// Target CSS file
+  input_file: String,
+  /// Destination file for the output
+  #[clap(short, long, group = "output_file")]
+  output_file: Option<String>,
+  /// Minify the output
+  #[clap(short, long)]
+  minify: bool,
+  /// Enable parsing CSS nesting
+  #[clap(short, long)]
+  nesting: bool,
+  /// Enable CSS modules in output
+  #[clap(short, long, group = "css_modules")]
+  css_modules: bool,
+  /// Default: <output_file>.json
+  #[clap(long, requires = "css_modules")]
+  css_modules_output_file: Option<String>,
+  /// Enable sourcemap, at <output_file>.map
+  #[clap(short, long, requires = "output_file")]
+  sourcemap: bool,
 }
 
 #[derive(Serialize)]
@@ -35,103 +35,108 @@ struct SourceMapJson<'a> {
   mappings: String,
   sources: &'a Vec<String>,
   sources_content: &'a Vec<String>,
-  names: &'a Vec<String>
+  names: &'a Vec<String>,
 }
 
 pub fn main() -> Result<(), std::io::Error> {
-    let cli_args = CliArgs::parse();
-    let source = fs::read_to_string(&cli_args.input_file)?;
+  let cli_args = CliArgs::parse();
+  let source = fs::read_to_string(&cli_args.input_file)?;
 
-    let filename = path::Path::new(&cli_args.input_file)
-        .file_name()
-        .unwrap()
-        .to_str()
-        .unwrap()
-        .into();
-    let mut stylesheet = StyleSheet::parse(
-        filename,
-        &source,
-        ParserOptions {
-            nesting: cli_args.nesting,
-            css_modules: cli_args.css_modules,
-            ..ParserOptions::default()
-        },
-    )
+  let filename = path::Path::new(&cli_args.input_file)
+    .file_name()
+    .unwrap()
+    .to_str()
+    .unwrap()
+    .into();
+  let mut stylesheet = StyleSheet::parse(
+    filename,
+    &source,
+    ParserOptions {
+      nesting: cli_args.nesting,
+      css_modules: cli_args.css_modules,
+      ..ParserOptions::default()
+    },
+  )
+  .unwrap();
+
+  if cli_args.minify {
+    stylesheet.minify(MinifyOptions::default()).unwrap();
+  }
+
+  let mut res = stylesheet
+    .to_css(PrinterOptions {
+      minify: cli_args.minify,
+      source_map: cli_args.sourcemap,
+      ..PrinterOptions::default()
+    })
     .unwrap();
 
-    if cli_args.minify {
-        stylesheet.minify(MinifyOptions::default()).unwrap();
-    }
+  let map = if let Some(ref mut source_map) = res.source_map {
+    source_map
+      .set_source_content(0, &res.code)
+      .map_err(|_| io::Error::new(io::ErrorKind::Other, "Error setting sourcemap"))?;
+    let mut vlq_output: Vec<u8> = Vec::new();
+    source_map
+      .write_vlq(&mut vlq_output)
+      .map_err(|_| io::Error::new(io::ErrorKind::Other, "Error writing sourcemap vlq"))?;
 
-    let mut res = stylesheet
-        .to_css(PrinterOptions {
-            minify: cli_args.minify,
-            source_map: cli_args.sourcemap,
-            ..PrinterOptions::default()
-        })
-        .unwrap();
-
-    let map = if let Some(ref mut source_map) = res.source_map {
-        source_map.set_source_content(0, &res.code).map_err(|_| io::Error::new(io::ErrorKind::Other, "Error setting sourcemap"))?;
-        let mut vlq_output: Vec<u8> = Vec::new();
-        source_map.write_vlq(&mut vlq_output).map_err(|_| io::Error::new(io::ErrorKind::Other, "Error writing sourcemap vlq"))?;
-
-        let sm = SourceMapJson {
-            version: 3,
-            mappings: unsafe { String::from_utf8_unchecked(vlq_output) },
-            sources: source_map.get_sources(),
-            sources_content: source_map.get_sources_content(),
-            names: source_map.get_names()
-        };
-
-        serde_json::to_vec(&sm).ok()
-    } else {
-        None
+    let sm = SourceMapJson {
+      version: 3,
+      mappings: unsafe { String::from_utf8_unchecked(vlq_output) },
+      sources: source_map.get_sources(),
+      sources_content: source_map.get_sources_content(),
+      names: source_map.get_names(),
     };
-    if let Some(output_file) = &cli_args.output_file {
-        let mut code = res.code;
-        if cli_args.sourcemap {
-            if let Some(map_buf) = map {
-                let map_filename: String = output_file.to_owned() + ".map";
-                code += &format!("\n/*# sourceMappingURL={} */\n", map_filename);
-                fs::write(map_filename, map_buf)?;
-            }
-        }
 
-        fs::write(output_file, code.as_bytes())?;
-
-        if cli_args.css_modules {
-            let css_modules_filename = cli_args
-                .css_modules_output_file
-                .unwrap_or(infer_css_modules_filename(cli_args.output_file)?);
-            if let Some(exports) = res.exports {
-                let css_modules_json = serde_json::to_string(&exports)?;
-                fs::write(css_modules_filename, css_modules_json)?;
-            }
-        }
-
-
-    } else {
-        println!("{}", res.code);
-        if cli_args.css_modules {
-            let css_modules_json = serde_json::to_string(&res.exports)?;
-            println!("{}", css_modules_json);
-        }
+    serde_json::to_vec(&sm).ok()
+  } else {
+    None
+  };
+  if let Some(output_file) = &cli_args.output_file {
+    let mut code = res.code;
+    if cli_args.sourcemap {
+      if let Some(map_buf) = map {
+        let map_filename: String = output_file.to_owned() + ".map";
+        code += &format!("\n/*# sourceMappingURL={} */\n", map_filename);
+        fs::write(map_filename, map_buf)?;
+      }
     }
 
-    Ok(())
+    fs::write(output_file, code.as_bytes())?;
+
+    if cli_args.css_modules {
+      let css_modules_filename = cli_args
+        .css_modules_output_file
+        .unwrap_or(infer_css_modules_filename(cli_args.output_file)?);
+      if let Some(exports) = res.exports {
+        let css_modules_json = serde_json::to_string(&exports)?;
+        fs::write(css_modules_filename, css_modules_json)?;
+      }
+    }
+  } else {
+    println!("{}", res.code);
+    if cli_args.css_modules {
+      let css_modules_json = serde_json::to_string(&res.exports)?;
+      println!("{}", css_modules_json);
+    }
+  }
+
+  Ok(())
 }
 
 fn infer_css_modules_filename(output_file: Option<String>) -> Result<String, std::io::Error> {
-    if let Some(file) = output_file {
-        let path = path::Path::new(&file);
-        if path.extension() == Some(ffi::OsStr::new("json")) {
-            Err(io::Error::new(io::ErrorKind::Other, "Cannot infer a css modules json filename, since the output file extension is '.json'"))
-        } else {
-            // unwrap: the filename option is a String from clap, so is valid utf-8
-            Ok(path.with_extension("json").to_str().unwrap().into())
-        }
+  if let Some(file) = output_file {
+    let path = path::Path::new(&file);
+    if path.extension() == Some(ffi::OsStr::new("json")) {
+      Err(io::Error::new(
+        io::ErrorKind::Other,
+        "Cannot infer a css modules json filename, since the output file extension is '.json'",
+      ))
     } else {
-        Ok("styles.json".into())
+      // unwrap: the filename option is a String from clap, so is valid utf-8
+      Ok(path.with_extension("json").to_str().unwrap().into())
     }
+  } else {
+    Ok("styles.json".into())
+  }
 }
--- tests/cli_integration_tests.rs
+++ tests/cli_integration_tests.rs
@@ -8,62 +8,62 @@ use std::collections::HashMap;
 use std::process::Command;
 
 fn test_file() -> Result<assert_fs::NamedTempFile, FixtureError> {
-    let file = assert_fs::NamedTempFile::new("test.css")?;
-    file.write_str(
-        r#"
+  let file = assert_fs::NamedTempFile::new("test.css")?;
+  file.write_str(
+    r#"
       .foo {
         border: none;
       }
     "#,
-    )?;
-    Ok(file)
+  )?;
+  Ok(file)
 }
 
 fn css_module_test_vals() -> (String, String, String) {
-    let exports: HashMap<&str, CssModuleExport> = HashMap::from([
-        (
-            "fade",
-            CssModuleExport {
-                name: "fade_EgL3uq".into(),
-                composes: vec![],
-                is_referenced: false,
-            },
-        ),
-        (
-            "foo",
-            CssModuleExport {
-                name: "foo_EgL3uq".into(),
-                composes: vec![],
-                is_referenced: false,
-            },
-        ),
-        (
-            "circles",
-            CssModuleExport {
-                name: "circles_EgL3uq".into(),
-                composes: vec![],
-                is_referenced: true,
-            },
-        ),
-        (
-            "id",
-            CssModuleExport {
-                name: "id_EgL3uq".into(),
-                composes: vec![],
-                is_referenced: false,
-            },
-        ),
-        (
-            "test",
-            CssModuleExport {
-                name: "test_EgL3uq".into(),
-                composes: vec![],
-                is_referenced: true,
-            },
-        ),
-    ]);
+  let exports: HashMap<&str, CssModuleExport> = HashMap::from([
     (
-        r#"
+      "fade",
+      CssModuleExport {
+        name: "fade_EgL3uq".into(),
+        composes: vec![],
+        is_referenced: false,
+      },
+    ),
+    (
+      "foo",
+      CssModuleExport {
+        name: "foo_EgL3uq".into(),
+        composes: vec![],
+        is_referenced: false,
+      },
+    ),
+    (
+      "circles",
+      CssModuleExport {
+        name: "circles_EgL3uq".into(),
+        composes: vec![],
+        is_referenced: true,
+      },
+    ),
+    (
+      "id",
+      CssModuleExport {
+        name: "id_EgL3uq".into(),
+        composes: vec![],
+        is_referenced: false,
+      },
+    ),
+    (
+      "test",
+      CssModuleExport {
+        name: "test_EgL3uq".into(),
+        composes: vec![],
+        is_referenced: true,
+      },
+    ),
+  ]);
+  (
+    r#"
       .foo {
         color: red;
       }
@@ -90,8 +90,8 @@ fn css_module_test_vals() -> (String, String, String) {
         to { opacity: 1 }
       }
     "#
-        .into(),
-        indoc! {r#"
+    .into(),
+    indoc! {r#"
       .foo_EgL3uq {
         color: red;
       }
@@ -128,105 +128,102 @@ fn css_module_test_vals() -> (String, String, String) {
         }
       }
     "#}
-        .into(),
-        serde_json::to_string(&exports).unwrap(),
-    )
+    .into(),
+    serde_json::to_string(&exports).unwrap(),
+  )
 }
 
 #[test]
 fn valid_input_file() -> Result<(), Box<dyn std::error::Error>> {
-    let file = assert_fs::NamedTempFile::new("test.css")?;
-    file.write_str(
-        r#"
+  let file = assert_fs::NamedTempFile::new("test.css")?;
+  file.write_str(
+    r#"
       .foo {
         border: none;
       }
     "#,
-    )?;
+  )?;
 
-    let mut cmd = Command::cargo_bin("parcel_css")?;
-    cmd.arg(file.path());
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::contains(indoc! {r#"
+  let mut cmd = Command::cargo_bin("parcel_css")?;
+  cmd.arg(file.path());
+  cmd.assert().success().stdout(predicate::str::contains(indoc! {r#"
         .foo {
           border: none;
         }"#}));
 
-    Ok(())
+  Ok(())
 }
 
 #[test]
 fn no_input_file() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("parcel_css")?;
-    cmd.assert().failure().stderr(predicate::str::contains(
-        "The following required arguments were not provided:\n    <INPUT_FILE>",
-    ));
+  let mut cmd = Command::cargo_bin("parcel_css")?;
+  cmd.assert().failure().stderr(predicate::str::contains(
+    "The following required arguments were not provided:\n    <INPUT_FILE>",
+  ));
 
-    Ok(())
+  Ok(())
 }
 
 #[test]
 fn empty_input_file() -> Result<(), Box<dyn std::error::Error>> {
-    let file = assert_fs::NamedTempFile::new("test.css")?;
-    file.write_str("")?;
+  let file = assert_fs::NamedTempFile::new("test.css")?;
+  file.write_str("")?;
 
-    let mut cmd = Command::cargo_bin("parcel_css")?;
-    cmd.arg(file.path());
-    cmd.assert().success();
+  let mut cmd = Command::cargo_bin("parcel_css")?;
+  cmd.arg(file.path());
+  cmd.assert().success();
 
-    Ok(())
+  Ok(())
 }
 
 #[test]
 fn output_file_option() -> Result<(), Box<dyn std::error::Error>> {
-    let infile = test_file()?;
-    let outfile = assert_fs::NamedTempFile::new("test.out")?;
-    let mut cmd = Command::cargo_bin("parcel_css")?;
-    cmd.arg(infile.path());
-    cmd.arg("--output-file").arg(outfile.path());
-    cmd.assert().success();
-    outfile.assert(predicate::str::contains(indoc! {r#"
+  let infile = test_file()?;
+  let outfile = assert_fs::NamedTempFile::new("test.out")?;
+  let mut cmd = Command::cargo_bin("parcel_css")?;
+  cmd.arg(infile.path());
+  cmd.arg("--output-file").arg(outfile.path());
+  cmd.assert().success();
+  outfile.assert(predicate::str::contains(indoc! {r#"
         .foo {
           border: none;
         }"#}));
 
-    Ok(())
+  Ok(())
 }
 
 #[test]
 fn minify_option() -> Result<(), Box<dyn std::error::Error>> {
-    let infile = test_file()?;
-    let mut cmd = Command::cargo_bin("parcel_css")?;
-    cmd.arg(infile.path());
-    cmd.arg("--minify");
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::contains(indoc! {r#".foo{border:0}"#}));
+  let infile = test_file()?;
+  let mut cmd = Command::cargo_bin("parcel_css")?;
+  cmd.arg(infile.path());
+  cmd.arg("--minify");
+  cmd
+    .assert()
+    .success()
+    .stdout(predicate::str::contains(indoc! {r#".foo{border:0}"#}));
 
-    Ok(())
+  Ok(())
 }
 
 #[test]
 // nesting doesn't do anything with the default targets. until cli supports more targets, this option is a noop
 #[ignore]
 fn nesting_option() -> Result<(), Box<dyn std::error::Error>> {
-    let infile = assert_fs::NamedTempFile::new("test.css")?;
-    infile.write_str(
-        r#"
+  let infile = assert_fs::NamedTempFile::new("test.css")?;
+  infile.write_str(
+    r#"
         .foo {
           color: blue;
           & > .bar { color: red; }
         }
       "#,
-    )?;
+  )?;
 
-    let mut cmd = Command::cargo_bin("parcel_css")?;
-    cmd.arg(infile.path());
-    cmd.arg("--nesting");
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::contains(indoc! {r#"
+  let mut cmd = Command::cargo_bin("parcel_css")?;
+  cmd.arg(infile.path());
+  cmd.arg("--nesting");
+  cmd.assert().success().stdout(predicate::str::contains(indoc! {r#"
         .foo {
           color: #00f;
         }
@@ -236,94 +233,93 @@ fn nesting_option() -> Result<(), Box<dyn std::error::Error>> {
         }
       "#}));
 
-    Ok(())
+  Ok(())
 }
 
 #[test]
 fn css_modules_stdout() -> Result<(), Box<dyn std::error::Error>> {
-    let (input, output, exports) = css_module_test_vals();
-    let infile = assert_fs::NamedTempFile::new("test.css")?;
-    infile.write_str(&input)?;
-    let mut cmd = Command::cargo_bin("parcel_css")?;
-    cmd.arg(infile.path());
-    cmd.arg("--css-modules");
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::contains(output));
+  let (input, output, exports) = css_module_test_vals();
+  let infile = assert_fs::NamedTempFile::new("test.css")?;
+  infile.write_str(&input)?;
+  let mut cmd = Command::cargo_bin("parcel_css")?;
+  cmd.arg(infile.path());
+  cmd.arg("--css-modules");
+  cmd.assert().success().stdout(predicate::str::contains(output));
 
-    let assert = cmd.assert();
-    let output = String::from_utf8(assert.get_output().stdout.clone())?;
-    let module_json_line = output.lines().next_back().unwrap();
-    let expected: serde_json::Value = serde_json::from_str(&exports)?;
-    let actual: serde_json::Value = serde_json::from_str(&module_json_line)?;
-    assert_eq!(expected, actual);
+  let assert = cmd.assert();
+  let output = String::from_utf8(assert.get_output().stdout.clone())?;
+  let module_json_line = output.lines().next_back().unwrap();
+  let expected: serde_json::Value = serde_json::from_str(&exports)?;
+  let actual: serde_json::Value = serde_json::from_str(&module_json_line)?;
+  assert_eq!(expected, actual);
 
-    Ok(())
+  Ok(())
 }
 
 #[test]
 fn css_modules_infer_output_file() -> Result<(), Box<dyn std::error::Error>> {
-    let (input, _, exports) = css_module_test_vals();
-    let infile = assert_fs::NamedTempFile::new("test.css")?;
-    let outfile = assert_fs::NamedTempFile::new("out.css")?;
-    infile.write_str(&input)?;
-    let mut cmd = Command::cargo_bin("parcel_css")?;
-    cmd.arg(infile.path());
-    cmd.arg("--css-modules");
-    cmd.arg("-o").arg(outfile.path());
-    cmd.assert()
-        .success();
+  let (input, _, exports) = css_module_test_vals();
+  let infile = assert_fs::NamedTempFile::new("test.css")?;
+  let outfile = assert_fs::NamedTempFile::new("out.css")?;
+  infile.write_str(&input)?;
+  let mut cmd = Command::cargo_bin("parcel_css")?;
+  cmd.arg(infile.path());
+  cmd.arg("--css-modules");
+  cmd.arg("-o").arg(outfile.path());
+  cmd.assert().success();
 
-    let expected: serde_json::Value = serde_json::from_str(&exports)?;
-    let actual: serde_json::Value = serde_json::from_str(&std::fs::read_to_string(
-        outfile.path().with_extension("json"),
-    )?)?;
-    assert_eq!(expected, actual);
+  let expected: serde_json::Value = serde_json::from_str(&exports)?;
+  let actual: serde_json::Value =
+    serde_json::from_str(&std::fs::read_to_string(outfile.path().with_extension("json"))?)?;
+  assert_eq!(expected, actual);
 
-    Ok(())
+  Ok(())
 }
 
 #[test]
 fn css_modules_output_target_option() -> Result<(), Box<dyn std::error::Error>> {
-    let (input, _, exports) = css_module_test_vals();
-    let infile = assert_fs::NamedTempFile::new("test.css")?;
-    let outfile = assert_fs::NamedTempFile::new("out.css")?;
-    let modules_file = assert_fs::NamedTempFile::new("module.json")?;
-    infile.write_str(&input)?;
-    let mut cmd = Command::cargo_bin("parcel_css")?;
-    cmd.arg(infile.path());
-    cmd.arg("-o").arg(outfile.path());
-    cmd.arg("--css-modules");
-    cmd.arg("--css-modules-output-file")
-        .arg(modules_file.path());
-    cmd.assert().success();
+  let (input, _, exports) = css_module_test_vals();
+  let infile = assert_fs::NamedTempFile::new("test.css")?;
+  let outfile = assert_fs::NamedTempFile::new("out.css")?;
+  let modules_file = assert_fs::NamedTempFile::new("module.json")?;
+  infile.write_str(&input)?;
+  let mut cmd = Command::cargo_bin("parcel_css")?;
+  cmd.arg(infile.path());
+  cmd.arg("-o").arg(outfile.path());
+  cmd.arg("--css-modules");
+  cmd.arg("--css-modules-output-file").arg(modules_file.path());
+  cmd.assert().success();
 
-    let expected: serde_json::Value = serde_json::from_str(&exports)?;
-    let actual: serde_json::Value =
-        serde_json::from_str(&std::fs::read_to_string(modules_file.path())?)?;
-    assert_eq!(expected, actual);
+  let expected: serde_json::Value = serde_json::from_str(&exports)?;
+  let actual: serde_json::Value = serde_json::from_str(&std::fs::read_to_string(modules_file.path())?)?;
+  assert_eq!(expected, actual);
 
-    Ok(())
+  Ok(())
 }
 
 #[test]
 fn sourcemap() -> Result<(), Box<dyn std::error::Error>> {
-    let (input, _, _) = css_module_test_vals();
-    let infile = assert_fs::NamedTempFile::new("test.css")?;
-    let outdir = assert_fs::TempDir::new()?;
-    let outfile = outdir.child("out.css");
-    infile.write_str(&input)?;
-    let mut cmd = Command::cargo_bin("parcel_css")?;
-    cmd.arg(infile.path());
-    cmd.arg("-o").arg(outfile.path());
-    cmd.arg("--sourcemap");
-    cmd.assert().success();
+  let (input, _, _) = css_module_test_vals();
+  let infile = assert_fs::NamedTempFile::new("test.css")?;
+  let outdir = assert_fs::TempDir::new()?;
+  let outfile = outdir.child("out.css");
+  infile.write_str(&input)?;
+  let mut cmd = Command::cargo_bin("parcel_css")?;
+  cmd.arg(infile.path());
+  cmd.arg("-o").arg(outfile.path());
+  cmd.arg("--sourcemap");
+  cmd.assert().success();
 
-    outfile.assert(predicate::str::contains(&format!("/*# sourceMappingURL={}.map */", outfile.path().to_str().unwrap())));
-    let mapfile = outdir.child("out.css.map");
-    mapfile.assert(predicate::str::contains(r#""version":3"#));
-    mapfile.assert(predicate::str::contains(r#""sources":["test.css"]"#));
-    mapfile.assert(predicate::str::contains(r#""mappings":"AACM;;;;AAIA;;;;AAIA;;;;;;;;;;AAKA;;;;AAIA;;;;AAIA""#));
+  outfile.assert(predicate::str::contains(&format!(
+    "/*# sourceMappingURL={}.map */",
+    outfile.path().to_str().unwrap()
+  )));
+  let mapfile = outdir.child("out.css.map");
+  mapfile.assert(predicate::str::contains(r#""version":3"#));
+  mapfile.assert(predicate::str::contains(r#""sources":["test.css"]"#));
+  mapfile.assert(predicate::str::contains(
+    r#""mappings":"AACM;;;;AAIA;;;;AAIA;;;;;;;;;;AAKA;;;;AAIA;;;;AAIA""#,
+  ));
 
-    Ok(())
+  Ok(())
 }
