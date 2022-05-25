dc983501c4544d324ceeefc0f20fe8707f7cc3a4
Devon Govett <devongovett@gmail.com>
2022-05-24 23:17:00 -0400

    Fix flow types

--- scripts/build-flow.js
+++ scripts/build-flow.js
@@ -5,9 +5,10 @@ let index = fs.readFileSync(dir + '/node/index.d.ts', 'utf8');
 index = '// @flow\n' + index;
 index = index.replace(/export interface (.*?) \{((?:.|\n)*?)\}/g, 'export type $1 = {|$2|};');
 index = index.replace(/export declare function/g, 'declare export function');
+index = index.replace("Omit<TransformOptions, 'code'>", "$Rest<TransformOptions, {|code: TransformOptions['code']|}>");
 
 let targets = fs.readFileSync(dir + '/node/targets.d.ts', 'utf8');
 targets = targets.replace(/export interface (.*?) \{((?:.|\n)*?)\}/g, 'export type $1 = {|$2|};');
-index = index.replace("import type {Targets} from './targets';", targets);
+index = index.replace(/import type \{\s*Targets\s*\} from '.\/targets';/, targets);
 
 fs.writeFileSync(dir + '/node/index.js.flow', index);
