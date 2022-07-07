d004c2fd3d04e2386fc4151320b457f54b023f19
Devon Govett <devongovett@gmail.com>
2022-07-07 09:10:56 -0700

    Add support for node esm
    Closes #220

---
+++ node/index.mjs
@@ -0,0 +1,4 @@
+import index from './index.js';
+
+const { transform, transformStyleAttribute, bundle, browserslistToTargets } = index;
+export { transform, transformStyleAttribute, bundle, browserslistToTargets };
