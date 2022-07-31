33130d8444b5c0dd48963bdfdeef4a33a0591332
Devon Govett <devongovett@gmail.com>
2022-07-31 14:17:02 -0700

    Fix flow build

--- scripts/build-flow.js
+++ scripts/build-flow.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 let dir = `${__dirname}/../`;
 let index = fs.readFileSync(dir + '/node/index.d.ts', 'utf8');
 index = '// @flow\n' + index;
-index = index.replace(/export interface (.*?) \{((?:.|\n)*?)\}/g, 'export type $1 = {|$2|};');
+index = index.replace(/export interface (.*?)(?: extends (.+?))? \{((?:.|\n)*?)\}/g, (_, name, ext, contents) => `export type ${name} =${ext ? ` ${ext} &` : ''} {|${contents}|};`);
 index = index.replace(/export declare function/g, 'declare export function');
 index = index.replace("Omit<TransformOptions, 'code'>", "$Rest<TransformOptions, {|code: TransformOptions['code']|}>");
 
