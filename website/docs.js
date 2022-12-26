0f1193696815577930a970f48778483d0603c668
Devon Govett <devongovett@gmail.com>
2022-12-26 10:18:52 -0500

    Use IntersectionObserver

--- website/docs.js
+++ website/docs.js
@@ -1,26 +1,38 @@
 // Mark the current section in the table of contents with aria-current when scrolled into view.
 let tocLinks = document.querySelectorAll('.table-of-contents a');
-let headers = [];
+let headers = new Map();
 for (let link of tocLinks) {
   let headerId = link.hash.slice(1);
   let header = document.getElementById(headerId);
-  headers.push({ link, header });
+  headers.set(header, link);
 }
 
-function updateToc() {
-  for (let header of headers) {
-    if ((header.header.offsetTop + header.header.getBoundingClientRect().height) > document.documentElement.scrollTop) {
-      let currentSelection = document.querySelector('.table-of-contents a[aria-current]');
-      if (currentSelection) {
-        currentSelection.removeAttribute('aria-current');
-      }
-      header.link.setAttribute('aria-current', 'location');
-      break;
+let intersectingHeaders = new Set();
+let observer = new IntersectionObserver(entries => {
+  for (let entry of entries) {
+    if (entry.isIntersecting) {
+      intersectingHeaders.add(entry.target);
+    } else {
+      intersectingHeaders.delete(entry.target);
     }
   }
-}
 
-updateToc();
-document.addEventListener('scroll', () => {
-  requestAnimationFrame(updateToc);
+  if (intersectingHeaders.size > 0) {
+    let current = document.querySelector('.table-of-contents a[aria-current]');
+    if (current) {
+      current.removeAttribute('aria-current');
+    }
+    let first;
+    for (let [header, link] of headers) {
+      if (intersectingHeaders.has(header)) {
+        first = link;
+        break;
+      }
+    }
+    first.setAttribute('aria-current', 'location');
+  }
 });
+
+for (let header of headers.keys()) {
+  observer.observe(header);
+}
