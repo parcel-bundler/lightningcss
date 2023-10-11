986828cbcf29fb76a406c841d2cb472c01dcfa9e
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-11 16:25:34 +0900

    move

--- src/traits.rs
+++ src/traits.rs
@@ -10,64 +10,6 @@ use crate::targets::{Browsers, Targets};
 use crate::vendor_prefix::VendorPrefix;
 use cssparser::*;
 
-/// A trait for things that can be cloned with a new lifetime.
-///
-/// `'any` lifeitme means the output should have `'static` lifetime.
-#[cfg(feature = "into_owned")]
-pub trait IntoOwned<'any> {
-  /// A variant of `Self` with a new lifetime.
-  type Owned: 'any;
-
-  /// Make lifetime of `self` `'static`.
-  fn into_owned(self) -> Self::Owned;
-}
-
-#[cfg(feature = "into_owned")]
-macro_rules! impl_into_owned {
-  ($t: ty) => {
-    impl<'a> IntoOwned<'a> for $t {
-      type Owned = Self;
-
-      #[inline]
-      fn into_owned(self) -> Self {
-        self
-      }
-    }
-  };
-  ($($t:ty),*) => {
-    $(impl_into_owned!($t);)*
-  };
-}
-
-#[cfg(feature = "into_owned")]
-impl_into_owned!(bool, f32, f64, u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, usize, isize);
-
-#[cfg(feature = "into_owned")]
-impl<'any, T> IntoOwned<'any> for Vec<T>
-where
-  T: for<'aa> IntoOwned<'aa>,
-{
-  type Owned = Vec<<T as IntoOwned<'any>>::Owned>;
-
-  fn into_owned(self) -> Self::Owned {
-    self.into_iter().map(|v| v.into_owned()).collect()
-  }
-}
-
-#[cfg(feature = "into_owned")]
-impl<'any, T, const N: usize> IntoOwned<'any> for SmallVec<[T; N]>
-where
-  T: for<'aa> IntoOwned<'aa>,
-  [T; N]: smallvec::Array<Item = T>,
-  [<T as IntoOwned<'any>>::Owned; N]: smallvec::Array<Item = <T as IntoOwned<'any>>::Owned>,
-{
-  type Owned = SmallVec<[<T as IntoOwned<'any>>::Owned; N]>;
-
-  fn into_owned(self) -> Self::Owned {
-    self.into_iter().map(|v| v.into_owned()).collect()
-  }
-}
-
 /// Trait for things that can be parsed from CSS syntax.
 pub trait Parse<'i>: Sized {
   /// Parse a value of this type using an existing parser.
--- static-self/src/lib.rs
+++ static-self/src/lib.rs
@@ -1,14 +1,53 @@
-pub fn add(left: usize, right: usize) -> usize {
-  left + right
+/// A trait for things that can be cloned with a new lifetime.
+///
+/// `'any` lifeitme means the output should have `'static` lifetime.
+pub trait IntoOwned<'any> {
+  /// A variant of `Self` with a new lifetime.
+  type Owned: 'any;
+
+  /// Make lifetime of `self` `'static`.
+  fn into_owned(self) -> Self::Owned;
 }
 
-#[cfg(test)]
-mod tests {
-  use super::*;
+macro_rules! impl_into_owned {
+  ($t: ty) => {
+    impl<'a> IntoOwned<'a> for $t {
+      type Owned = Self;
 
-  #[test]
-  fn it_works() {
-    let result = add(2, 2);
-    assert_eq!(result, 4);
+      #[inline]
+      fn into_owned(self) -> Self {
+        self
+      }
+    }
+  };
+  ($($t:ty),*) => {
+    $(impl_into_owned!($t);)*
+  };
+}
+
+impl_into_owned!(bool, f32, f64, u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, usize, isize);
+
+impl<'any, T> IntoOwned<'any> for Vec<T>
+where
+  T: for<'aa> IntoOwned<'aa>,
+{
+  type Owned = Vec<<T as IntoOwned<'any>>::Owned>;
+
+  fn into_owned(self) -> Self::Owned {
+    self.into_iter().map(|v| v.into_owned()).collect()
+  }
+}
+
+#[cfg(feature = "smallvec")]
+impl<'any, T, const N: usize> IntoOwned<'any> for SmallVec<[T; N]>
+where
+  T: for<'aa> IntoOwned<'aa>,
+  [T; N]: smallvec::Array<Item = T>,
+  [<T as IntoOwned<'any>>::Owned; N]: smallvec::Array<Item = <T as IntoOwned<'any>>::Owned>,
+{
+  type Owned = SmallVec<[<T as IntoOwned<'any>>::Owned; N]>;
+
+  fn into_owned(self) -> Self::Owned {
+    self.into_iter().map(|v| v.into_owned()).collect()
   }
 }
