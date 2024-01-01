d7ae958a71bb8b6aa0d25fb08bb6afd84f3a9259
Romain Menke <romainmenke@gmail.com>
2024-01-01 15:33:34 +0100

    improve bench

--- bench.js
+++ bench.js
@@ -1,5 +1,7 @@
 const css = require('./');
 const cssnano = require('cssnano');
+const postcssMinify = require('@csstools/postcss-minify');
+const postcssPresetEnv = require('postcss-preset-env');
 const postcss = require('postcss');
 const esbuild = require('esbuild');
 
@@ -9,37 +11,76 @@ let opts = {
   minify: true,
   // source_map: true,
   targets: {
-    chrome: 95 << 16
+    chrome: 80 << 16,
+    firefox: 80 << 16,
+    safari: 12 << 16,
   }
 };
 
-async function run() {
-  await doCssNano();
+async function bench(label, job) {
+  let results = [];
+  let percentageOfOriginalSize = 0;
+  for (let i = 0; i < 100; i++) {
+    const start = performance.now();
+    percentageOfOriginalSize = await job();
+    results.push(performance.now() - start);
+  }
 
-  console.time('esbuild');
-  let r = await esbuild.transform(opts.code.toString(), {
-    sourcefile: opts.filename,
-    loader: 'css',
-    minify: true
-  });
-  console.timeEnd('esbuild');
-  console.log(r.code.length + ' bytes');
-  console.log('');
+  results.sort();
 
-  console.time('lightningcss');
-  let res = css.transform(opts);
-  console.timeEnd('lightningcss');
-  console.log(res.code.length + ' bytes');
+  return {
+    label: label,
+    percentile50: `${results[49].toFixed(1)}ms`,
+    percentile95: `${results[Math.floor(results.length * 0.95)].toFixed(1)}ms`,
+    percentile99: `${results[98].toFixed(1)}ms`,
+    max: `${results[99].toFixed(1)}ms`,
+    percentageOfOriginalSize: `${percentageOfOriginalSize.toFixed(2)}%`
+  };
 }
 
-async function doCssNano() {
-  console.time('cssnano');
-  const result = await postcss([
-    cssnano,
-  ]).process(opts.code, {from: opts.filename});
-  console.timeEnd('cssnano');
-  console.log(result.css.length + ' bytes');
-  console.log('');
+async function sleep(duration) {
+  return new Promise(resolve => setTimeout(resolve, duration));
+}
+
+async function run() {
+  const cssnanoResult = await bench('cssnano', async () => {
+    const result = await postcss([
+      cssnano({ presets: [{ mergeLonghand : false} ] }),
+    ]).process(opts.code, { from: opts.filename });
+    return result.css.length / opts.code.length * 100;
+  });
+
+  await sleep(500);
+
+  const postcssMinifyResult = await bench('postcss-preset-env + @csstools/postcss-minify', async () => {
+    const result = await postcss([
+      postcssPresetEnv({
+        browsers: 'Chrome 80, Firefox 80, Safari 12',
+      }),
+      postcssMinify,
+    ]).process(opts.code, { from: opts.filename });
+    return result.css.length / opts.code.length * 100;
+  });
+
+  await sleep(500);
+
+  const esbuildResult = await bench('esbuild', async () => {
+    let r = await esbuild.transform(opts.code.toString(), {
+      sourcefile: opts.filename,
+      loader: 'css',
+      minify: true
+    });
+    return r.code.length / opts.code.length * 100;
+  });
+
+  await sleep(500);
+
+  const lightningcssResult = await bench('lightningcss', async () => {
+    let res = css.transform(opts);
+    return res.code.length / opts.code.length * 100;
+  });
+
+  console.table([cssnanoResult, postcssMinifyResult, esbuildResult, lightningcssResult]);
 }
 
 run();
