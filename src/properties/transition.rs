dae7a621cf48abeb9445e893e4d0fc77db29582e
Devon Govett <devongovett@gmail.com>
2024-11-24 20:37:30 -0800

    Handle more cases

--- src/properties/transition.rs
+++ src/properties/transition.rs
@@ -155,28 +155,14 @@ impl<'i> PropertyHandler<'i> for TransitionHandler<'i> {
 
     match property {
       TransitionProperty(val, vp) => {
-        let none_prefixed_names = val
-          .iter()
-          .filter(|p| p.prefix() == VendorPrefix::None)
-          .map(|p| p.name())
-          .collect::<Vec<_>>();
-
-        if none_prefixed_names.is_empty() {
-          property!(TransitionProperty, properties, val, vp)
-        } else {
-          let filtered_val = val
-            .iter()
-            .filter(|p| !(none_prefixed_names.contains(&p.name()) && p.prefix() != VendorPrefix::None))
-            .cloned()
-            .collect::<SmallVec<[PropertyId<'_>; 1]>>();
-          property!(TransitionProperty, properties, &filtered_val, vp)
-        }
+        let merged_values = merge_properties(val.iter());
+        property!(TransitionProperty, properties, &merged_values, vp);
       }
       TransitionDuration(val, vp) => property!(TransitionDuration, durations, val, vp),
       TransitionDelay(val, vp) => property!(TransitionDelay, delays, val, vp),
       TransitionTimingFunction(val, vp) => property!(TransitionTimingFunction, timing_functions, val, vp),
       Transition(val, vp) => {
-        let properties: SmallVec<[PropertyId; 1]> = val.iter().map(|b| b.property.clone()).collect();
+        let properties: SmallVec<[PropertyId; 1]> = merge_properties(val.iter().map(|b| &b.property));
         maybe_flush!(properties, &properties, vp);
 
         let durations: SmallVec<[Time; 1]> = val.iter().map(|b| b.duration.clone()).collect();
@@ -343,6 +329,23 @@ fn is_transition_property(property_id: &PropertyId) -> bool {
     | PropertyId::Transition(_) => true,
     _ => false,
   }
+}
+
+fn merge_properties<'i: 'a, 'a>(val: impl Iterator<Item = &'a PropertyId<'i>>) -> SmallVec<[PropertyId<'i>; 1]> {
+  let mut merged_values = SmallVec::<[PropertyId<'_>; 1]>::with_capacity(val.size_hint().1.unwrap_or(1));
+  for p in val {
+    let without_prefix = p.with_prefix(VendorPrefix::empty());
+    if let Some(idx) = merged_values
+      .iter()
+      .position(|c| c.with_prefix(VendorPrefix::empty()) == without_prefix)
+    {
+      merged_values[idx].add_prefix(p.prefix());
+    } else {
+      merged_values.push(p.clone());
+    }
+  }
+
+  merged_values
 }
 
 fn expand_properties<'i>(
