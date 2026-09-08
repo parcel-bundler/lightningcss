8e481afd402a4c4e23c797e6165c859c27008b80
Cong-Cong Pan <dacongsama@live.com>
2026-09-08 15:32:21 +0800

    chore: keep fixture license notice inline

--- src/lib.rs
+++ src/lib.rs
@@ -10395,8 +10395,30 @@ mod tests {
 
   // Cases adapted from cssnano/postcss-merge-rules (MIT), with declarations
   // changed to custom properties where appropriate:
-  // The upstream fixture license is preserved in tests/cssnano/LICENSE.
   // https://github.com/cssnano/cssnano/blob/13b2373173ce49c78b8a2015d8c3f6b025ccfc3e/packages/postcss-merge-rules/test/index.js
+  //
+  // Copyright (c) Ben Briggs <beneb.info@gmail.com> (https://beneb.info)
+  //
+  // Permission is hereby granted, free of charge, to any person
+  // obtaining a copy of this software and associated documentation
+  // files (the "Software"), to deal in the Software without
+  // restriction, including without limitation the rights to use,
+  // copy, modify, merge, publish, distribute, sublicense, and/or sell
+  // copies of the Software, and to permit persons to whom the
+  // Software is furnished to do so, subject to the following
+  // conditions:
+  //
+  // The above copyright notice and this permission notice shall be
+  // included in all copies or substantial portions of the Software.
+  //
+  // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+  // OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  // NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+  // HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+  // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  // OTHER DEALINGS IN THE SOFTWARE.
   #[test]
   fn test_merge_common_custom_properties_cssnano() {
     // Different declaration order (cssnano test/index.js:40).
